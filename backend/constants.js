a3fbdada3944847167a130077129672496b58872
Vera Anyasova <vera.anyasova1@yandex.ru>
2023-05-31 10:54:52 +0500

    изменить порт

--- backend/constants.js
+++ backend/constants.js
@@ -4,8 +4,6 @@ const regex =
 const allowedCors = [
   "https://vera.anyasova.students.nomoredomains.monster",
   "http://vera.anyasova.students.nomoredomains.monster",
-  "https://localhost:3000",
-  "http://localhost:3000",
   "https://localhost:3001",
   "http://localhost:3001",
   "https://51.250.88.144",
